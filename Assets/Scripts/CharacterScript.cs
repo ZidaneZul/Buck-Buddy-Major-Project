1e7b430eb7afe400c6c1d13ffade3c346ee0beab
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-04 14:18:44 +0800

    some code

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
+    float horizontal;
+    float speed = 3f;
+    bool isFacingRight = true;
+
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,20 @@ public class CharacterScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(horizontal * speed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+        }
     }
 }
