20642e0aa2d3e4b0a8fc26529b531fb1281bb561
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-07-04 15:41:21 +0800

    AISpawning

--- Assets/Scripts/Gameplay Script/NPCRandomChance.cs
+++ Assets/Scripts/Gameplay Script/NPCRandomChance.cs
@@ -7,31 +7,77 @@ public class NPCRandomChance : MonoBehaviour
 {
     Scene currentScene;
     string SceneName;
+    NPCRandomChance randomChance;
+    public GameObject[] NPC;
 
-    
-    // Start is called before the first frame update
+    // To preload variables that will stay between transition of scenes
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if(randomChance == null)
+        {
+            randomChance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     void Start()
     {
         currentScene = SceneManager.GetActiveScene();
         SceneName = currentScene.name;  
         StartCoroutine(NPCMaker(SceneName));    
-        
+    
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        
+        SceneChanged();
+        TemporaryButton();
     }
 
     IEnumerator NPCMaker(string NPCtoSpawn)
     {
+        if(NPCtoSpawn == "SampleScene")
+        {
+            SceneManager.LoadScene("Level1");
+        }
         if (NPCtoSpawn == "Level1")
         {
+            int randomNPC = Random.Range(0, NPC.Length);
             Debug.Log("A random NPC Appears");
+            Instantiate(NPC[randomNPC]);
             yield return new WaitForSeconds(3f);
-            StopAllCoroutines();
+            StopCoroutine(NPCMaker(NPCtoSpawn));
+        }
+    }
+    
+
+    void SceneChanged()
+    {
+        if(SceneManager.GetActiveScene() != currentScene)
+        {
+            currentScene = SceneManager.GetActiveScene();
+            SceneName = currentScene.name;
+            StartCoroutine(NPCMaker(SceneName));
+        }
+
+    }
+
+    void TemporaryButton()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level1");
+
         }
     }
 }
