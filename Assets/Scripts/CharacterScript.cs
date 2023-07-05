21ee0beab5c0be63465e2c672c5f48c7e4fc633a
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-05 12:22:48 +0800

    Clean folders

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -6,7 +6,7 @@ public class CharacterScript : MonoBehaviour
 {
     float horizontal;
     float speed = 3f;
-    bool isFacingRight = true;
+    bool isFacingRight = true;  
 
     Rigidbody rb;
     // Start is called before the first frame update
