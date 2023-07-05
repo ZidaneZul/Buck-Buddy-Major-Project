8ea6167a4621c84789b9ea2bd4bfb61e7787f701
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-05 16:15:55 +0800

    cacat positioning of cash payment

--- Assets/Scripts/Cash Register Scripts/PaymentDrop.cs
+++ Assets/Scripts/Cash Register Scripts/PaymentDrop.cs
@@ -5,12 +5,24 @@ using UnityEngine.EventSystems;
 
 public class PaymentDrop : MonoBehaviour, IDropHandler
 {
+    public GameObject[] placement;
+    int i;
+    public void Start()
+    {
+        placement = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+    public void Update()
+    {
+        Debug.Log(i);
+    }
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log("Dropped"); 
         if(eventData.pointerDrag != null)
         {
-            eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+           //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
+            eventData.pointerDrag.GetComponent<RectTransform>().transform.position = placement[i].transform.position;
+            i++;
         }
     }
 }
