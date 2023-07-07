6823db0e0ec9d1ddf487b38e79184403232ede3b
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-07-07 13:35:06 +0800

    NPC Full Done

--- Assets/Assets/Scripts/Gameplay Script/NPCRandomChance.cs
+++ Assets/Assets/Scripts/Gameplay Script/NPCRandomChance.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.AI;
+using TMPro;
 public class NPCRandomChance : MonoBehaviour
 {
     Scene currentScene;
     string SceneName;
     NPCRandomChance randomChance;
     public GameObject[] NPC;
+    public GameObject NPCOnScene;
+    public string[] Dialogues;
     public GameObject[] SpawnPoints;
     public NavMeshAgent nma;
     public GameObject Player;
@@ -18,6 +21,11 @@ public class NPCRandomChance : MonoBehaviour
     public CharacterScript character;
     public Collider[] collidedObjects;
     public bool interacted;
+    public int randomNPC;
+    public GameObject dialogueBox;
+    public GameObject dialogueBoxInScene;
+    public TextMeshPro NPCText;
+    public bool yes;
 
     // To preload variables that will stay between transition of scenes
 
@@ -47,16 +55,25 @@ public class NPCRandomChance : MonoBehaviour
 
     void Update()
     {
+        if(GameObject.FindGameObjectWithTag("NPC") != null)
+        {
+            GotoNextPoint();
+        }
+        if ((PlayerDetection()))
+        {
+            TestDialogue();
+        }
+       
 
-        GotoNextPoint();
         SceneChanged();
         TemporaryButton();
+        
 
     }
     void FixedUpdate()
     {
 
-        if (GameObject.FindGameObjectWithTag("NPC"))
+        if (NPCOnScene != null)
         {
             collidedObjects = Physics.OverlapSphere(nma.transform.position, 3f);
         }
@@ -71,14 +88,18 @@ public class NPCRandomChance : MonoBehaviour
         }
         if (NPCtoSpawn == "Level1")
         {
-            int randomNPC = Random.Range(0, NPC.Length);
+            randomNPC = Random.Range(0, NPC.Length);
             SpawnPointLocation = Random.Range(0, SpawnPoints.Length);
-
             Debug.Log("A random NPC Appears");
             Instantiate(NPC[randomNPC],SpawnPoints[SpawnPointLocation].transform);
-            nma = GameObject.FindGameObjectWithTag("NPC").GetComponent<NavMeshAgent>();
+            Instantiate(dialogueBox);
+            NPCOnScene = GameObject.FindGameObjectWithTag("NPC");
+            nma = NPCOnScene.GetComponent<NavMeshAgent>();
             nma.stoppingDistance = 0.25f;
+            dialogueBoxInScene = GameObject.FindGameObjectWithTag("textBox");
+            NPCText = GameObject.FindGameObjectWithTag("textBox").GetComponentInChildren<TextMeshPro>();
             yield return new WaitForSeconds(3f);
+            dialogueBoxInScene.SetActive(false);
             StopAllCoroutines();
 
         }
@@ -103,10 +124,11 @@ public class NPCRandomChance : MonoBehaviour
     {
         if (!PlayerDetection())
         {
-            if (GameObject.FindGameObjectWithTag("NPC"))
+            if (NPCOnScene != null)
             {
                 if (SpawnPointLocation == 0)
                 {
+                    dialogueBoxInScene.SetActive(false);
                     for (int i = 0; i < 2; i++)
                     {
                         if (target == null)
@@ -133,7 +155,7 @@ public class NPCRandomChance : MonoBehaviour
 
                 if (SpawnPointLocation % 2 == 0)
                 {
-
+                    dialogueBoxInScene.SetActive(false);
                     for (int i = 0; i < 2; i++)
                     {
                         if (target == null)
@@ -161,6 +183,7 @@ public class NPCRandomChance : MonoBehaviour
 
                 if (SpawnPointLocation % 2 == 1)
                 {
+                    dialogueBoxInScene.SetActive(false);
                     for (int i = 0; i < 2; i++)
                     {
                         if (target == null)
@@ -194,6 +217,7 @@ public class NPCRandomChance : MonoBehaviour
         {
             nma.isStopped = true;
             character.speed = 0f;
+
             Transform characterDirection = character.transform;
 
             if (nma.transform.position.x < character.transform.position.x)
@@ -207,6 +231,7 @@ public class NPCRandomChance : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Y))
                 {
                     interacted = true;
+                    yes = true;
                     nma.isStopped = false;
                     character.transform.localScale = characterDirection.transform.localScale;
                     character.speed = 3f;
@@ -214,8 +239,9 @@ public class NPCRandomChance : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.N))
                 {
                     interacted = true;
+                    yes = false;
+                    nma.isStopped = false;
                     character.transform.localScale = characterDirection.transform.localScale;
-                    nma.gameObject.SetActive(false);
                     character.speed = 3f;
                 }
             }
@@ -230,6 +256,7 @@ public class NPCRandomChance : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.Y))
                 {
                     interacted = true;
+                    yes = true;
                     character.transform.localScale = characterDirection.transform.localScale;
                     nma.isStopped = false;
                     character.speed = 3f;
@@ -238,8 +265,9 @@ public class NPCRandomChance : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.N))
                 {
                     interacted = true;
+                    yes = false;
+                    nma.isStopped = false;
                     character.transform.localScale = characterDirection.transform.localScale;
-                    nma.gameObject.SetActive(false);
                     character.speed = 3f;
                 }
             }
@@ -247,12 +275,49 @@ public class NPCRandomChance : MonoBehaviour
 
     }
 
+    void TestDialogue()
+    {
+        int NPCTypeHolder = 0;
+        dialogueBoxInScene.SetActive(true);
+        dialogueBoxInScene.transform.rotation.SetLookRotation(Camera.main.transform.position);
+        dialogueBoxInScene.transform.position = new Vector3(NPCOnScene.transform.position.x, 5f, NPCOnScene.transform.position.z);
+
+
+        for (int i = 0; i < NPC.Length; i++)
+        {
+            Debug.Log(i);
+
+            if (i == randomNPC)
+            {
+                NPCTypeHolder = i;
+                NPCText.text = Dialogues[i*3];
+
+
+            }
+
+        }
+
+        if (interacted == true && yes == true)
+        {
+            NPCText.text = Dialogues[NPCTypeHolder * 3 + 1];
+
+        }
+        if (interacted == true && yes == false)
+        {
+            NPCText.text = Dialogues[NPCTypeHolder * 3 + 2 ];
+
+        }
+
+    }
+
+
     
 
     void SceneChanged()
     {
         if(SceneManager.GetActiveScene() != currentScene)
         {
+            StopAllCoroutines();
             currentScene = SceneManager.GetActiveScene();
             SceneName = currentScene.name;
             SpawnPoints = GameObject.FindGameObjectsWithTag("Spawners");
