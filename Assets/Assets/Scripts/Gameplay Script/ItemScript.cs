ce352f1b7ca21ea077e3edeb7c4be88ca0c042f4
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-11 16:06:17 +0800

    almost finished food bubble

--- Assets/Assets/Scripts/Gameplay Script/ItemScript.cs
+++ Assets/Assets/Scripts/Gameplay Script/ItemScript.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class ItemScript : MonoBehaviour
 {
     public GameObject infoBubble;
-    GameObject infoBubbleClone;
-    Vector3 foodBubblePos;
+    GameObject foodBubbleClone;
+    
 
     GameObject[] foodPoints;
     GameObject tempPoint, pointChildren;
+    Vector3 foodBubblePos;
     string pointChildrenString;
 
-    bool didTextSpawn = false;
 
-    Vector3 lowerPadding = new Vector3(0, -5, 0);
+    bool didTextSpawn = false;
 
     // Start is called before the first frame update
     void Start()
@@ -26,11 +26,11 @@ public class ItemScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        IsClose();
+        IsCloseToPoint();
 
     }
 
-    void IsClose()
+    void IsCloseToPoint()
     {
         foreach (GameObject point in foodPoints)
         {
@@ -38,29 +38,27 @@ public class ItemScript : MonoBehaviour
             {
                 tempPoint = point;
                 Debug.Log("Found a close point");
-                //ShowBubble();
-                while(Vector3.Distance(transform.position, tempPoint.transform.position) < 3)
-                {
-                    ShowBubble();
-                }
+                IsCloseToFood();
             }
             else
             {
                 DeleteBubble();
             }
         }
-
-        //if(Vector3.Distance(transform.position, tempPoint.transform.position) < 3)
-        //{
-        //    ShowBubble();
-        //    Debug.Log("Close to " + tempPoint);
-        //}
-        //else
-        //{
-        //    DeleteBubble();
-        //}
     }
 
+    void IsCloseToFood()
+    {
+        if (Vector3.Distance(transform.position, tempPoint.transform.position) < 3)
+        {
+            ShowBubble();
+            Debug.Log("Close to " + tempPoint);
+        }
+        else
+        {
+            DeleteBubble();
+        }
+    }
     void ShowBubble()
     {
         //pointChildrenString = tempPoint.GetComponentInChildren<GameObject>().ToString();
@@ -69,18 +67,29 @@ public class ItemScript : MonoBehaviour
         pointChildrenString = pointChildren.ToString();
         Debug.Log("the child object is " + pointChildren);
 
-        foodBubblePos = new Vector3 (pointChildren.transform.position.x, pointChildren.transform.position.y - 1.4f, pointChildren.transform.position.z);
+        //foodBubblePos.position = new Vector3(pointChildren.transform.position.x, pointChildren.transform.position.y - 1.4f, pointChildren.transform.position.z);
+        
+
 
         if (!didTextSpawn)
         {
             didTextSpawn = true;
-            infoBubbleClone = Instantiate(infoBubble, pointChildren.transform);
+
+            foodBubblePos = pointChildren.transform.position;
+            foodBubblePos.y = pointChildren.transform.position.y - 1.4f;
+
+            foodBubbleClone = Instantiate(infoBubble);
+
+            foodBubbleClone.transform.position = foodBubblePos;
+            Debug.Log("pos of point is " + pointChildren.transform.position + "\n" +
+            "pos of food bubble is " + foodBubblePos);
+
         }
     }
 
     void DeleteBubble()
     {
-        Destroy(infoBubbleClone);
+        Destroy(foodBubbleClone);
         didTextSpawn=false;
     }
         
