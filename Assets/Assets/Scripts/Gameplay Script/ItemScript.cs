eb14a385b239d732ec421c6dac3e48df9aaef396
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-11 15:02:20 +0800

    havent test if it workds

--- Assets/Assets/Scripts/Gameplay Script/ItemScript.cs
+++ Assets/Assets/Scripts/Gameplay Script/ItemScript.cs
@@ -6,10 +6,15 @@ public class ItemScript : MonoBehaviour
 {
     public GameObject infoBubble;
     GameObject infoBubbleClone;
+    Vector3 foodBubblePos;
 
     GameObject[] foodPoints;
     GameObject tempPoint, pointChildren;
     string pointChildrenString;
+
+    bool didTextSpawn = false;
+
+    Vector3 lowerPadding = new Vector3(0, -5, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -32,22 +37,51 @@ public class ItemScript : MonoBehaviour
             if (Vector3.Distance(transform.position, point.transform.position) < 3)
             {
                 tempPoint = point;
-                Debug.Log("Close to point!");
-                ShowBubble();
+                Debug.Log("Found a close point");
+                //ShowBubble();
+                while(Vector3.Distance(transform.position, tempPoint.transform.position) < 3)
+                {
+                    ShowBubble();
+                }
+            }
+            else
+            {
+                DeleteBubble();
             }
         }
+
+        //if(Vector3.Distance(transform.position, tempPoint.transform.position) < 3)
+        //{
+        //    ShowBubble();
+        //    Debug.Log("Close to " + tempPoint);
+        //}
+        //else
+        //{
+        //    DeleteBubble();
+        //}
     }
 
     void ShowBubble()
     {
-        pointChildrenString = tempPoint.GetComponentInChildren<GameObject>().ToString();
+        //pointChildrenString = tempPoint.GetComponentInChildren<GameObject>().ToString();
         
         pointChildren = tempPoint.transform.GetChild(0).gameObject;
         pointChildrenString = pointChildren.ToString();
         Debug.Log("the child object is " + pointChildren);
 
-        infoBubbleClone = Instantiate(infoBubble, pointChildren.transform);
+        foodBubblePos = new Vector3 (pointChildren.transform.position.x, pointChildren.transform.position.y - 1.4f, pointChildren.transform.position.z);
 
-
+        if (!didTextSpawn)
+        {
+            didTextSpawn = true;
+            infoBubbleClone = Instantiate(infoBubble, pointChildren.transform);
+        }
     }
+
+    void DeleteBubble()
+    {
+        Destroy(infoBubbleClone);
+        didTextSpawn=false;
+    }
+        
 }
