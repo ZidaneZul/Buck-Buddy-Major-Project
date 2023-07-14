d9e84c95467568fb9be230f32bbdb943c5b39196
ZidaneZul <2100498A@student.tp.edu.sg>
2023-07-14 16:30:16 +0800

    idk idkd dijoidoifjaofiawjfeoiwjf

--- Assets/Assets/Scripts/Data/InventoryManager.cs
+++ Assets/Assets/Scripts/Data/InventoryManager.cs
@@ -6,13 +6,16 @@ using TMPro;
 
 public class InventoryManager : MonoBehaviour
 {
-
     public static InventoryManager Instance;
-    public List<ItemData> Items = new List<ItemData>();
 
-    TextMeshProUGUI TMP;
+    public List<ItemData> itemList = new List<ItemData>();
+    List<int> idList = new List<int>();
+    Dictionary<int, int> idDiction = new Dictionary<int, int>();
 
-    string listOfCart;
+    public GameObject textPrefab;
+    Transform itemContent;
+
+    bool isCartOpen =false;
 
     private void Awake()
     {
@@ -21,28 +24,122 @@ public class InventoryManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TMP = GameObject.Find("CartBody_Txt").GetComponent<TextMeshProUGUI>();
+        itemContent = GameObject.Find("Content").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Items.Count);
+        //Debug.Log(itemList.Count);
     }
     public void Add(ItemData item)
     {
-        Items.Add(item);
+        itemList.Add(item);
     }
 
     public void ShowItem()
     {
-        listOfCart = null;
-        Debug.Log("Triggered");
-        foreach (var item in Items)
+        foreach (Transform item in itemContent)
         {
-            listOfCart += item.itemName + "\n";
-            //listOfCart.
-        }   
-        TMP.text = listOfCart;
+            Destroy(item.gameObject);
+        }
+
+        FindIDs();
+        //Dictionary<int, int> duplicateCounts = CountDuplicates(idList);
+
+
+
+        foreach (var item in itemList)
+        {
+
+            //foreach (KeyValuePair<int, int> pair in duplicateCounts)
+            //{
+            //Debug.Log("Element: " + pair.Key + " - Count: " + pair.Value);
+            //if (pair.Key == item.id)
+            //{
+
+
+
+            //}
+            // }
+                GameObject textObj = Instantiate(textPrefab, itemContent);
+                var itemName = textObj.transform.Find("FoodName_Txt").GetComponent<TextMeshProUGUI>();
+                var itemPrice = textObj.transform.Find("FoodPrice_Txt").GetComponent<TextMeshProUGUI>();
+                var itemQuantity = textObj.transform.Find("Quantity_Txt").GetComponent<TextMeshProUGUI>();
+
+                itemName.text = item.itemName;
+                itemPrice.text = "$" + item.price.ToString();
+        }
     }
+
+
+    
+    //Dictionary<int, int> CountDuplicates(List<int> CDlist)
+    //{
+    //    Dictionary<int, int> duplicateCounts = new Dictionary<int, int>();
+
+    //    foreach (int element in CDlist)
+    //    {
+    //        if (duplicateCounts.ContainsKey(element))
+    //        {
+    //            duplicateCounts[element]++;
+    //        }
+    //        else
+    //        {
+    //            duplicateCounts[element] = 1;
+    //        }
+    //    }
+
+    //    return duplicateCounts;
+    //}    
+    
+    void FindIDs()
+    {
+        if (!isCartOpen)
+        {
+            isCartOpen = true;
+            foreach(var item in itemList)
+            {
+                idList.Add(item.id);
+            }
+        }
+        else
+        {
+            isCartOpen = false;
+            idList.Clear();
+        }
+    }
+    
+    //void FindDuplicates()
+    //{
+    //    if (!isCartOpen)
+    //    {
+    //        isCartOpen = true;
+    //        Debug.Log("Cart is now open");
+
+    //        foreach (var item in itemList)
+    //        {
+    //            testList.Add(item.id);
+    //            if (uniqueIdList.Contains(item.id))
+    //            {
+    //               // Debug.Log("Duplicate found!");
+    //                duplicatedList.Add(item.id);
+    //               // Debug.Log("item " + item.id + "has be found ");
+    //            }
+    //            else
+    //            {
+    //                //Debug.Log("not a dup!");
+    //                uniqueIdList.Add(item.id);
+    //            }
+    //            //Debug.Log("Unique id: " + uniqueIdList.ToString());
+    //        }
+    //    }
+    //    else
+    //    {
+    //        isCartOpen = false;
+    //        uniqueIdList.Clear();
+    //        testList.Clear();
+    //        Debug.Log("cart is now close");
+    //    }
+    //}
 }
