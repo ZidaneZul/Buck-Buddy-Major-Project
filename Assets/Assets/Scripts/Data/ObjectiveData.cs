1f55742f04c9ba0ebdd993f9add9db98303f324b
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-07-18 11:12:31 +0800

    Dataset

---
+++ Assets/Assets/Scripts/Data/ObjectiveData.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Objective", menuName = "ObjectiveData/Create New Objective")]
+public class ObjectiveData : ScriptableObject
+{
+    public int Soup;
+    public int Lettuce;
+    public int Cheese;
+    public int Tomatoes;
+    public int Noodles;
+    public int Eggs;
+    public int Bread;
+    public int Mushrooms;
+    public int Meat;
+    public int SoySauce;
+    public int Ham;
+
+
+
+
+
+
+}
