af44a7c48542db0f6f3a8dbe1fdd68f601b6f248
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-07-25 09:36:16 +0800

    NPCDone

---
+++ Assets/Assets/Scripts/NPCDialogue/DialogueManager.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> dialogues;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+    public Sprite NPCImage;
+    public Animator animator;
+    public bool NPCInteracted;
+    public GameObject NPCDialogueBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogues = new Queue<string>();
+        NPCDialogueBox.SetActive(false);
+    }
+
+    public void NPCRandomChance(Dialogue dialogue)
+    {
+        if (NPCInteracted)
+        {
+            return;
+        }
+        else if(Random.Range(1,3) == 2)
+        {
+
+            StartDialogue(dialogue);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        NPCDialogueBox.SetActive(true);
+        animator.SetBool("IsOpen", true);
+        Debug.Log("Starting Conversation with" + dialogue.NPCname);
+        dialogues.Clear();
+        NPCImage = dialogue.NPCimage.sprite;
+        nameText.text = dialogue.NPCname;
+        foreach(string sentences in dialogue.dialogues)
+        {
+            dialogues.Enqueue(sentences);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if(dialogues.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = dialogues.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    public void EndDialogue()
+    {
+        NPCDialogueBox.SetActive(false);
+        animator.SetBool("IsOpen", false);
+    }
+}
