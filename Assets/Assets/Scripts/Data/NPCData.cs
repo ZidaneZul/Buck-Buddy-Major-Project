b5692122c71511dfd3f6a76060c3d6672358bfe1
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-07-26 13:07:47 +0800

    NPC

--- Assets/Assets/Scripts/Data/NPCData.cs
+++ Assets/Assets/Scripts/Data/NPCData.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New NPC", menuName = "NPCData/Create New NPC Data")]
 public class NPCData : ScriptableObject
 {
-    public string NPCname;
-    public Sprite NPCimage;
+    public string[] NPCname;
+    public Sprite[] NPCimage;
 
     [TextArea(3, 10)]
     public string[] dialogues;
     public string yesResponse;
     public string noResponse;
     public string[] NPCResponses;
+    public int LineToStopAt;
 }
--- Assets/Assets/Scripts/NPCDialogue/DialogueManager.cs
+++ Assets/Assets/Scripts/NPCDialogue/DialogueManager.cs
@@ -8,6 +8,8 @@ using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
     private Queue<string> dialogues;
+    private Queue<string> dialogueNames;
+    private Queue<Sprite> NpcImages;
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI dialogueText;
     public Image NPCImage;
@@ -27,6 +29,8 @@ public class DialogueManager : MonoBehaviour
     void Start()
     {
         dialogues = new Queue<string>();
+        dialogueNames = new Queue<string>();
+        NpcImages = new Queue<Sprite>();
         NPCDialogueBox.SetActive(false);
         npcData = NPCDataList[Random.Range(0, NPCDataList.Length)];
     }
@@ -58,26 +62,38 @@ public class DialogueManager : MonoBehaviour
         animator.SetBool("IsOpen", true);
         Debug.Log("Starting Conversation with" + npcData.NPCname);
         dialogues.Clear();
-        NPCImage.sprite = npcData.NPCimage;
-        nameText.text = npcData.NPCname;
         yesButton.SetActive(false);
         noButton.SetActive(false);
         foreach (string sentences in npcData.dialogues)
         {
             dialogues.Enqueue(sentences);
         }
+        foreach(string names in npcData.NPCname)
+        {
+            dialogueNames.Enqueue(names);
+        }
+        foreach(Sprite images in npcData.NPCimage)
+        {
+            NpcImages.Enqueue(images);
+        }
+
         DisplayNextSentence();
     }
     public void DisplayNextSentence()
     {
         if(dialogues.Count == 0)
+        //dialogues.Count - npcData.LineToStopAt
         {
             EndDialogue();
             return;
         }
         string sentence = dialogues.Dequeue();
+        string names = dialogueNames.Dequeue();
+        Sprite images = NpcImages.Dequeue();
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
+        nameText.text = names;
+        NPCImage.sprite = images;
     }
 
     IEnumerator TypeSentence(string sentence)
@@ -88,10 +104,12 @@ public class DialogueManager : MonoBehaviour
             dialogueText.text += letter;
             yield return null;
         }
+
     }
 
     public void EndDialogue()
     {
+
         yesButton.SetActive(true);
         noButton.SetActive(true);
         ContinueButton.SetActive(false);
@@ -105,6 +123,7 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = npcData.NPCResponses[0];
         StartCoroutine(CloseTimer());
 
+       
     }
 
     public void NoChoice()
