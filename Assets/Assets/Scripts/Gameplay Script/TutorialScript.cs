056d917d17faafc20d1ce097b61a731a0f96b332
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-08-08 10:50:52 +0800

    UI Button Fix

---
+++ Assets/Assets/Scripts/Gameplay Script/TutorialScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject GrayedBox;
+    public GameObject MapBtn;
+    public GameObject CartBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //StartCoroutine(StartTutorial());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //IEnumerator StartTutorial()
+    //{
+
+    //}
+}
