8f1fbc0558e6e8177227411374d5d699868fd7e7
ZidaneZul <2100498A@student.tp.edu.sg>
2023-08-11 14:40:48 +0800

    trying to find player in square

--- Assets/Assets/Scripts/UI Scripts/MapLocation.cs
+++ Assets/Assets/Scripts/UI Scripts/MapLocation.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MapLocation : MonoBehaviour
 {
+
+    public GameObject[] sections;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sections = GameObject.FindGameObjectsWithTag("Sections");
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        FindPlayer();
+    }
+
+    void FindPlayer()
+    {
+        foreach(GameObject section in sections)
+        {
+            Collider[] hit = Physics.OverlapBox(section.transform.position, transform.localScale / 2);
+            if (hit.Equals(player))
+            {
+                Debug.Log("Player is in " + section.name);
+            }
+        }
     }
 }
