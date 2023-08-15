c0364b250f5adae9845ed9851279621b21a8cb60
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-08-15 11:28:11 +0800

    NPC WIP

---
+++ Assets/Assets/Scripts/NPCDialogue/NPCSpawning.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawning : MonoBehaviour
+{
+    public GameObject Player,NPC;
+    public List<GameObject> NPCspawnerLocations;
+    public int numberHolder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        NPCspawnerLocations = new List<GameObject>(GameObject.FindGameObjectsWithTag("Waypoint"));
+
+        Player = GameObject.FindGameObjectWithTag("Player");
+        
+        FindClosestWaypoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FindClosestWaypoint()
+    {
+        float distanceToClosestWaypoint = Mathf.Infinity;
+        GameObject closestWaypoint = null;
+        foreach(GameObject waypoint in NPCspawnerLocations)
+        {
+            float distanceToPlayer = (waypoint.transform.position - Player.transform.position).sqrMagnitude;
+            if(distanceToPlayer < distanceToClosestWaypoint)
+            {
+                distanceToClosestWaypoint = distanceToPlayer;
+                closestWaypoint = waypoint;
+                NPCspawnerLocations.Remove(closestWaypoint);
+                SpawnNPC();
+
+
+            }
+
+
+        }
+    }
+
+    void SpawnNPC()
+    {
+        numberHolder = Random.Range(0, NPCspawnerLocations.Count);
+        Instantiate(NPC, NPCspawnerLocations[numberHolder].transform);
+    }
+}
