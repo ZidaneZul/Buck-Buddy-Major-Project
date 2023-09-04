1988309693ee77cbdf1e82db8dd450808cac07bd
Coodelez <83225516+Coodelez@users.noreply.github.com>
2023-09-04 11:51:50 +0800

    timer added

--- Assets/Assets/Scripts/Gameplay Script/GameOverScript.cs
+++ Assets/Assets/Scripts/Gameplay Script/GameOverScript.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameOverScript : MonoBehaviour
 {
     public Objective objectiveData;
     public float levelBudget;
     public GameObject gameOverMenu;
+    public float timeLeft;
+    public TextMeshProUGUI timerText;
 
 
     private void Start()
@@ -15,18 +18,40 @@ public class GameOverScript : MonoBehaviour
         objectiveData = GameObject.Find("GameManager").GetComponent<Objective>();
         levelBudget = objectiveData.GetBudget();
         gameOverMenu.SetActive(false);
+        timeLeft = 10;
     }
 
     private void Update()
     {
-        if (levelBudget <= 0)
+        if (levelBudget <= 0 || timeLeft <= 0)
         {
-            gameOverMenu.SetActive(true);
+            GameOver(true);
         }
+        if(timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            Timer(timeLeft);
+        }
+
         else
         {
-            gameOverMenu.SetActive(false);
+            GameOver(false);
         }
+    }
+    public void Timer(float currentTime)
+    {
+        currentTime += 1;
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+
+        timerText.text = string.Format("{0:00} : {1:00}", minutes, seconds);
+
+        
+    }
+
+    public void GameOver(bool lost)
+    {
+        gameOverMenu.SetActive(lost);
     }
 
     public void SelectNewLevel()
